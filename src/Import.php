f54484bb0d466afd37ec702f738f02af62ba52b6
Justin Vogt <mail@justin-vogt.de>
2024-08-15 14:28:15 +0200

    Update Import.php

--- src/Import.php
+++ src/Import.php
@@ -8,6 +8,19 @@ abstract class Import extends Sync
 {
     protected int $chunk_counter = 0;
     protected int $chunk_limit = 0;
+
+    /**
+	 * Split the data, wherever it comes from into chunks.
+	 * This function has to be implemented within each "Import".
+	 * The basic workflow is:
+	 * 	1. Get the source data
+	 *  2. Split the data into the smaller subsets called chunks.
+	 * 	3. Schedule chunks to of data using the Chunker.php trait
+	 * 
+	 * @return void
+	 */
+	abstract public function split_data_into_chunks():void;
+    
     /**
      * @param string $chunk_file_path
      * @throws Exception
