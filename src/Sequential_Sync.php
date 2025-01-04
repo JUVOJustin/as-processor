cc740dc85235d25bd3988a873e9d34e8ec6473ce
Justin Vogt <mail@justin-vogt.de>
2025-01-04 14:56:39 +0100

    Update chunk limit and dependency reference
    Increased the chunk limit for development to 70 in `Sync_Amazon_Articles.php` to optimize processing. Updated the `as-processor` dependency reference in `composer.lock` to the latest version for improved functionality and stability.

--- src/Sequential_Sync.php
+++ src/Sequential_Sync.php
@@ -101,7 +101,7 @@ abstract class Sequential_Sync extends Sync implements Syncable {
 		add_action(
 			$this->get_sync_name() . '/complete',
 			function () {
-				$this->cleanup_sync_data( $this->get_sync_data_name() );
+				$this->cleanup_sync_data();
 			},
 			999
 		);
